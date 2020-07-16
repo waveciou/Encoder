1e5f231157451dc1997ff30a8ca49be1d29dc040
丘惟夫 <waveciou@gmail.com>
2020-07-16 22:59:15 +0800

    first commit

---
+++ common/js/_develop/encoder.js
@@ -0,0 +1,137 @@
+; (function (Vue) {
+
+    const app = {
+        template: `
+            <div class="main">
+                <div class="select__control">
+                    <div class="select__fieldset">
+                        <input type="radio" name="select-control" id="encode" v-model="encode_selected" @change="clearHandler" :value="true">
+                        <label for="encode" title="Encode">Encode<span></span></label>
+                    </div>
+                    <div class="select__fieldset">
+                        <input type="radio" name="select-control" id="decode" v-model="encode_selected" @change="clearHandler" :value="false">
+                        <label for="decode" title="Decode">Decode<span></span></label>
+                    </div>
+                </div>
+
+                <div class="article">
+                    <label class="caption" for="input-area">Input :</label>
+                    <textarea id="input-area" class="textarea" :placeholder="inputAreaPlaceholder" v-model.trim="textInput"></textarea>
+                    </div>
+
+                <div class="row">
+                    <button class="btn" @click.stop="submitHandler" title="Submit">Submit</button>
+                    <button class="btn" @click.stop="clearHandler" title="Clear">Clear</button>
+                </div>
+
+                <div class="article">
+                    <label class="caption" for="output-area">Output :</label>
+                    <div id="output-area" class="textarea" v-cloak>{{ textOutput }}</div>
+                </div>
+            </div>
+        `,
+        data() {
+            return {
+                textInput: "",
+                textOutput: "",
+                encode_selected: true,
+                process: {
+                    digits: 6
+                }
+            }
+        },
+        methods: {
+            clearHandler() {
+                this.textInput = "";
+                this.textOutput = "";
+            },
+            submitHandler() {
+                if (typeof this.textInput !== "string") {
+                    return false;
+                } else {
+                    this.textOutput = this.computedCode();
+                }
+            },
+            computedCode() {
+                if (this.encode_selected === true) {
+                    return this.encodeHandler(this.textInput);
+                } else {
+                    return this.decodeHandler(this.textInput);
+                }
+            },
+            encodeHandler(payload) {
+                const strArray = payload.split("");
+                const resultArray = strArray.map((item) => {
+                    let unicode = `${item.charCodeAt(0)}`;
+                    let codeArray = unicode.split("");
+                    let sup = 0 - (codeArray.length - this.process.digits);
+
+                    for (let i = 0; i < sup; i++) {
+                        if (i === sup - 1) {
+                            let randomKey = this.getRandomArbitrary(1, 9);
+                            codeArray.unshift(`${randomKey}`);
+                        } else {
+                            codeArray.unshift("0");
+                        }
+                    }
+
+                    return codeArray.join("");
+                });
+
+                return resultArray.join("");
+            },
+            decodeHandler(payload) {
+                let isError = parseInt(payload) ? false : true;
+
+                if (isError === true) {
+                    return "error"
+                }
+
+                const strArray = payload.split("");
+                const codeArray = [];
+
+                for (let i = 0; i < strArray.length; i += this.process.digits) {
+                    codeArray.push(strArray.slice(i, i + this.process.digits).join(""));
+                }
+
+                const resultArray = codeArray.map((item) => {
+                    const cipherNumber = parseInt(item.slice(1, this.process.digits));
+                    const keyNumber = parseInt(item.slice(0, 1));
+
+                    if (isError === false) {
+                        isError = cipherNumber <= 65535 || cipherNumber >= 32 || cipherNumber === 10 ? false : true;
+                    }
+
+                    const plainText = String.fromCharCode(`${cipherNumber}`);
+                    return plainText;
+                });
+
+                const result = resultArray.join("").trim();
+
+                if (result === "") {
+                    isError = true;
+                }
+
+                return isError === true ? "error" : result;
+            },
+            getRandomArbitrary(min, max) {
+                return Math.floor(Math.random() * (max - min) + min);
+            }
+        },
+        computed: {
+            inputAreaPlaceholder() {
+                const textContent = {
+                    encode: "Please enter the text for Encode.",
+                    decode: "Please enter the text for Decode."
+                };
+
+                return this.encode_selected === true ? textContent["encode"] : textContent["decode"];
+            }
+        }
+    }
+
+    const vm = new Vue({
+        render: h => h(app)
+    }).$mount('#app');
+
+})(Vue);
---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+const gulp = require('gulp'),
+    babel = require('gulp-babel'),
+    gulpUglify = require('gulp-uglify'),
+    concat = require('gulp-concat'),
+    htmlmin = require('gulp-htmlmin');
+
+gulp.task('htmlmin', function () {
+    return gulp.src('./assets/*.html').pipe(htmlmin({
+        collapseWhitespace: true
+    })).pipe(gulp.dest('./'));
+});
+
+gulp.task('babel', () => {
+    return gulp.src([
+        './common/js/plugins/vue.min.js',
+        './common/js/plugins/axios.min.js',
+        './common/js/_develop/*.js'
+    ]).pipe(babel({
+        presets: [
+            ['env', {
+                loose: true,
+                modules: false,
+            }]
+        ]
+    })).pipe(gulpUglify()).pipe(concat('main.js')).pipe(gulp.dest('./common/js'));
+});
+
+gulp.task('default', gulp.parallel(['htmlmin', 'babel']));
