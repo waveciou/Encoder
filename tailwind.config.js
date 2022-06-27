d1fcb31329da2c5d0c429efa96860881f1cec58e
丘惟夫 <waveciou@gmail.com>
2022-06-27 13:57:07 +0800

    [dev] add tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/resources/*.{js,ts,jsx,tsx}',
+    './src/resources/**/*.{js,ts,jsx,tsx}',
+    './src/resources/**/*.{scss,module.scss}',
+  ],
+  theme: {
+    fontSize: {
+      small: '14px',
+      mobile: '16px',
+      desktop: '20px',
+    },
+    screens: {
+      small: '370px',
+      mobile: '480px',
+      tablet: '768px',
+      'tablet-lg': '798px',
+      desktop: '1025px',
+    },
+    colors: {
+      white: '#FFFFFF',
+      black: '#2C2C2C',
+      green: '#3BB763',
+      blue: '#02A4E0',
+      yellow: '#EFCE4A',
+    },
+    extend: {},
+  },
+  plugins: [],
+};
