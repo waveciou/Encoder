7cd4c9a05ff6794740400036371509769d54baee
丘惟夫 <waveciou@gmail.com>
2024-05-02 23:00:49 +0800

    docker set up

--- vite.config.ts
+++ vite.config.ts
@@ -59,4 +59,14 @@ export default defineConfig({
       },
     },
   },
+  preview: {
+    port: 8080,
+    strictPort: true,
+  },
+  server: {
+    port: 8080,
+    strictPort: true,
+    host: true,
+    origin: 'http://0.0.0.0:8080',
+  },
 });
